05285b42c90c9e979c39cf17764ed1f69824507b
vduseev <vagiz.d@gmail.com>
2019-12-29 02:52:34 +0300

    Main executable supporting imports

--- fate.sh
+++ fate.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
-# Parse arguments
-echo "parsed"
+# Stores directory where root fate scripts resides
+FATE_SCRIPT_ROOT_DIR=""
 
+function determine_script_location_dir() {
+    local source="${BASH_SOURCE[0]}"
+    # resolve $source until the file is no longer a symlink
+    while [ -h "$source" ]; do
+        FATE_SCRIPT_ROOT_DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+        source="$(readlink "$source")"
+        # if $source was a relative symlink, we need to resolve it relative
+        # to the path where the symlink file was located
+        [[ $source != /* ]] && source="$FATE_SCRIPT_ROOT_DIR/$source"
+    done
+    FATE_SCRIPT_ROOT_DIR="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+}
+
+determine_script_location_dir
+
+source "$FATE_SCRIPT_ROOT_DIR/fate/main.sh"
+
+main "$@"
